913647426062be64e1a7ba678bac60dd03f4b763
sszjxv6v7p-source <sszjxv6v7p@privaterelay.appleid.com>
2025-12-06 18:16:21 +0800

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,8 @@
+function setLang(lang){
+    document.querySelectorAll("[data-cn]").forEach(el=>{
+        el.innerHTML = lang === "cn" 
+            ? el.getAttribute("data-cn") 
+            : el.getAttribute("data-en");
+    });
+}
+setLang("cn");
