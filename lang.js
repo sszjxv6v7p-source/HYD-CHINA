4d2a1463d3dc3aff0d843b77649565110839c5bb
sszjxv6v7p-source <sszjxv6v7p@privaterelay.appleid.com>
2025-12-06 18:40:39 +0800

    Delete lang.js

--- lang.js
+++
@@ -1,8 +0,0 @@
-function setLang(lang){
-    document.querySelectorAll("[data-cn]").forEach(el=>{
-        el.innerHTML = lang === "cn" 
-            ? el.getAttribute("data-cn") 
-            : el.getAttribute("data-en");
-    });
-}
-setLang("cn");
